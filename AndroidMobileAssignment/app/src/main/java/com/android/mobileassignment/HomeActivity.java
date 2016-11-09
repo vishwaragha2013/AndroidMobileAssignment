dc08484596fe0b7f3d0f6546922587e0dc1dbc3f
M1025835 <vishwanath.ragha@mindtree.com>
2016-11-09 12:02:07 +0530

    committed designed ui to github.

--- AndroidMobileAssignment/app/src/main/java/com/android/mobileassignment/HomeActivity.java
+++ AndroidMobileAssignment/app/src/main/java/com/android/mobileassignment/HomeActivity.java
@@ -1,13 +1,46 @@
 package com.android.mobileassignment;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class HomeActivity extends AppCompatActivity {
+
+
+    @Bind(R.id.tv_name)
+    TextView tvName;
+    @Bind(R.id.edt_name)
+    EditText edtName;
+    @Bind(R.id.tv_location)
+    TextView tvLocation;
+    @Bind(R.id.tv_location_value)
+    TextView tvLocationValue;
+    @Bind(R.id.btn_submit)
+    Button btnSubmit;
+    @Bind(R.id.tv_last_submitted)
+    TextView tvLastSubmitted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        tvName.setPaintFlags(tvName.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+        tvLocation.setPaintFlags(tvLocation.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+
+    }
+
+
+    @OnClick(R.id.btn_submit)
+    public void onClick() {
     }
 }
